3e7fb7c5ff8b8ee216979514dd0f79bfdc1d2110
Pedro Rodriguez <pedro@pjrt.co>
2016-06-20 18:32:48 +0000

    MVP
    Works but the map exists inside the code, which isn't cool.

--- app/Main.hs
+++ app/Main.hs
@@ -1,34 +1,59 @@
 {-# LANGUAGE OverloadedStrings #-}
+module Main where
 
+import Data.Text (unpack)
 import Turtle
 import qualified Data.HashMap.Strict as M
+import Prelude hiding (FilePath)
+import System.Posix.Files (createSymbolicLink)
 
 main :: IO ()
 main = sh $ do
-  curr <- pwd
-  file <- basename <$> find (suffix "dot") curr
-  matchAndMove file
-
-
-matchAndMove :: FilePath -> Shell ()
-matchAndMove file = do
-    h <- home
-    Just l <- M.map file (fileMaps h)
-    inproc "ln" ["-s", asText file, asText l] empty
+    dots <- options "Dot-Linker" parsePath
+    cd dots
+    file <- filename <$> ls "./"
+    matchAndLink file
   where
+    parsePath = argPath "dots location" ""
+
+
+matchAndLink :: FilePath -> Shell ()
+matchAndLink dot = do
+    h <- home
+    let dotAsText = asText dot
+        targetM = M.lookup dotAsText (fileMaps h)
+    maybe (unmatchFile $ asText dot) matchedFile targetM
+  where
+    unmatchFile d = echo $ "No match found for dot " <> d <> ". Skipping..."
+    matchedFile target = do
+      targetExists <- testfile target
+      let targetAsText = asText target
+      if targetExists
+        then echo $ asText dot <> " already exists. Skipping..."
+        else do
+          realdot <- realpath dot
+          echo $ "Linking " <> asText realdot <> " -> " <> asText target
+          sln realdot target
+
     asText = either id id . toText
 
 
-fileMaps :: FilePath -> HashMap FilePath FilePath
-fileMaps home = M.fromList [
-             ("ctags",        fromHome ".ctags")
-           , ("gitconfigs",   fromHome ".gitconfig")
-           , ("git_template", fromHome ".git_template")
-           , ("vimrc",        fromHome ".vimrc")
-           , ("xmobarrc",     fromHome ".xmobarrc")
-           , ("xmonad.hs",    fromHome ".xmonad/xmonad.hs")
-           , ("Xdefaults",    fromHome ".Xdefaults")
-           , ("zshrc",        fromHome ".zshrc")
-           ]
+fileMaps :: FilePath -> M.HashMap Text FilePath
+fileMaps home =
+  M.fromList
+    [ ("ctags",        onHome ".ctags-2")
+    , ("gitconfig",    onHome ".gitconfig")
+    , ("git_template", onHome ".git_template")
+    , ("vimrc",        onHome ".vimrc")
+    , ("xmobarrc",     onHome ".xmobarrc")
+    , ("xmonad.hs",    onHome ".xmonad/xmonad.hs")
+    , ("Xdefaults",    onHome ".Xdefaults")
+    , ("zshrc",        onHome ".zshrc")
+    ]
   where
-    fromHome f = home <> f
+    onHome f = home <> f
+
+
+sln :: MonadIO io => FilePath -> FilePath -> io ()
+sln src target = liftIO $ createSymbolicLink (toText' src) (toText' target)
+  where toText' = unpack . either id id . toText
